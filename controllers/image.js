df8b03c19e84e88b60a61f6ad18ff431ca22518c
Arsal Mehran <arsalmehran@gmail.com>
2018-09-17 06:25:31 -0700

    added environment variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai'); 
 
 const app = new Clarifai.App({
-  apiKey: '4248a24dd8d940fdbcffa3793ed8b110'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
